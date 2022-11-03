bb018b3fadd3fa5968614e925043f7a6da679ffb
Mateo Nieto <mnieto@lendingpoint.com>
2022-11-02 22:35:41 -0500

    add cards

--- lib/widgets/Trasaction_list.dart
+++ lib/widgets/Trasaction_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class TransactionList extends StatefulWidget{
   @override
@@ -22,27 +23,38 @@ class _TransactionListState extends State<TransactionList>{
           child: Row(
             children: [
               Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.purple,
                     width: 2
                   )
                 ),
-                padding: EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
                 child: Text('\$${transaction.amount}',
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
                     color: Colors.purple
                   ),
                 ),
               ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(transaction.title, style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold
+                  ),),
+                  Text( DateFormat('yyyy-MM-DD').format(transaction.date), style: TextStyle(color: Colors.grey),),
+                ],
+              )
 
             ],
-          )
-        )
+          ),
+        );
       }).toList()
-    )
+    );
   }
 }
